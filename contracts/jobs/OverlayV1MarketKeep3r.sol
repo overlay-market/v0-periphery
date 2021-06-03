6042f1173c4b5e2e7fcd6baadf403bb85820f5fa
mikeyrf <mikeyrf@users.noreply.github.com>
2021-06-03 11:55:29 -0400

    rename to v1

--- contracts/jobs/OverlayV1MarketKeep3r.sol
+++ contracts/jobs/OverlayV1MarketKeep3r.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@overlay/v1-core/contracts/interfaces/IOverlayFactory.sol";
+import "@overlay/v1-core/contracts/interfaces/IOverlayV1Factory.sol";
 import "../interfaces/IKeep3rV1.sol";
 
-contract OverlayMarketKeep3r {
+contract OverlayV1MarketKeep3r {
     IKeep3rV1 public constant KP3R = IKeep3rV1(0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44);
-    IOverlayFactory public OVLF;
+    IOverlayV1Factory public OVLF;
     address public rewardsTo;
 
     modifier upkeep() {
-        require(KP3R.isKeeper(msg.sender), "OverlayMarketKeep3r: !keeper");
+        require(KP3R.isKeeper(msg.sender), "OverlayV1MarketKeep3r: !keeper");
         _;
         KP3R.worked(msg.sender);
     }
 
     constructor(address _factory, address _rewardsTo) {
-        OVLF = IOverlayFactory(_factory);
+        OVLF = IOverlayV1Factory(_factory);
         rewardsTo = _rewardsTo;
     }
 
